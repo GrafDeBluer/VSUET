f02499a1e2ce87a70061458c74b0d14d9e9700ee
GrafDeBluer <108233512+GrafDeBluer@users.noreply.github.com>
2022-06-26 20:34:29 +0300

    Add files via upload

---
+++ 1.1.java
@@ -0,0 +1,10 @@
+﻿
+public class main {
+	public static void main(String[] name) {
+        if (name.length > 0) {
+            System.out.print("Hello, " + name[0] + "!");
+        } else {
+            System.out.print("Hello, World!");
+        }
+    }
+}
